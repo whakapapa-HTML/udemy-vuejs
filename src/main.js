1170a6b6188b690a0a5976c8b7cc0481c1310160
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-06-06 23:49:43 +0900

    [Initial Commit]Vue.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import LikeNumber from "./LikeNumber.vue";
 
 Vue.config.productionTip = false
+Vue.component('LikeNumber',LikeNumber);
+
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
