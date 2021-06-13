c03aedd9c24bf2ef76341b634dfbd063c30f4160
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-06-13 21:06:14 +0900

    filter

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,26 @@
 <template>
  <div>
    <p v-border:solid.shadow="{width:'5px',  color: 'red'}">Home</p>
-   <h2>{{ title | upperCase }}</h2>
-   <p>{{ subTitle | upperCase }}</p>
+   <h2>{{ title | lowerCase | upperCase }}</h2>
+   <p>{{ subTitle | lowerCase }}</p>
+   <p >{{ number }}</p>
+   <button @click="number++">+1</button>
+   <CountNumber></CountNumber>
  </div>
 </template>
 
 <script>
+import CountNumber from "./CountNumber.vue";
+import { tokyoNumber } from "@/tokyoNumber"
 export default {
+  mixins: [ tokyoNumber ],
   data() {
     return {
       tmpData: "hello",
-      title: "Welcome to Tokyo",
-      subTitle: "Tokyo is a great city"
     };
+  },
+  components: {
+    CountNumber
   },
   directives: {
     border(el, binding) {
--- src/main.js
+++ src/main.js
@@ -5,8 +5,14 @@ import LikeNumber from "./components/LikeNumber.vue";
 Vue.config.productionTip = false
 Vue.component('LikeNumber',LikeNumber);
 Vue.filter("upperCase", function(value){
+  console.log("フィルタ");
   return value.toUpperCase();
-})
+});
+Vue.mixin({
+  created() {
+    console.log("global mixin");
+  }
+});
 
 new Vue({
   render: h => h(App)
